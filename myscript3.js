d6b4b638a1681dc3b3c52c96a08f4e3031836a8b
Uzair Raza <uzair18it@gmail.com>
2024-10-03 05:02:06 +0500

    Assignment 4

---
+++ myscript3.js
@@ -0,0 +1,10 @@
+document.write("<h1>Rules for naming JS Variables</h1>");
+document.write("<br>");
+document.write("<br>");
+document.write("Variable names can only contain,numbers,$and_.For example: $my_1stVariable");
+document.write("<br>");
+document.write("Variables must begin with a letter, $ or _.For example $name, _name or name");
+document.write("<br>");
+document.write("Variable names are case sensitive");
+document.write("<br>");
+document.write("Variable names should not be JS keywords");
